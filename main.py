5ebedf180632edc017f8c5b1eb0183a686954f18
El Koulali András <deak311andris@gmail.com>
2024-02-08 13:42:22 +0100

    bigyula

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print("otl")
+import pygame
+
+win_width = 1000
+win_height = 600
+win = pygame.display.set_mode((win_width, win_height))
+
+running = True
+clock = pygame.time.Clock()
+
+while running:
+    for ev in pygame.event.get():
+        match ev.type:
+            case pygame.QUIT:
+                running = False
+            case pygame.KEYDOWN:
+                if ev.key == pygame.K_ESCAPE:
+                    running = False
+    
+    time = clock.get_time()
+    pygame.display.update()
+    clock.tick(60)
+
+pygame.quit()
